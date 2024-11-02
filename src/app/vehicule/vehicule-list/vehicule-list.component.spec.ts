2d0fc69f84255006a8b14d9fbf58026291ec1766
5x5x5-cube <ae.pena@uniandes.edu.co>
2024-11-02 01:05:43 -0500

    test: implemented vehicule-list tests

--- src/app/vehicule/vehicule-list/vehicule-list.component.spec.ts
+++ src/app/vehicule/vehicule-list/vehicule-list.component.spec.ts
@@ -1,28 +1,63 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 
 import { VehiculeListComponent } from './vehicule-list.component';
+import { VehiculeService } from '../vehicule.service';
+import { Vehicule } from '../vehicule';
 
 describe('VehiculeListComponent', () => {
   let component: VehiculeListComponent;
   let fixture: ComponentFixture<VehiculeListComponent>;
+  let debug: DebugElement;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehiculeListComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule],
+      declarations: [VehiculeListComponent],
+      providers: [VehiculeService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculeListComponent);
     component = fixture.componentInstance;
+    component.vehicules = Array.from(
+      { length: 3 },
+      () =>
+        new Vehicule({
+          id: faker.number.int(),
+          marca: faker.word.words(),
+          linea: faker.word.words(2),
+          referencia: faker.word.words(2),
+          modelo: faker.date.past().getFullYear(),
+          kilometraje: faker.number.int(),
+          color: faker.color.human(),
+          imagen: faker.image.url(),
+        })
+    );
+
+    console.log('component', component.vehicules.length);
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Component has a table', () => {
+    expect(debug.query(By.css('table'))).not.toBeNull();
+  });
+
+  it('Table has headers', () => {
+    expect(debug.query(By.css('thead')).childNodes.length).toEqual(1);
+  });
+
+  it('Table has rows', () => {
+    expect(debug.queryAll(By.css('tbody>tr')).length).toEqual(3);
   });
 });
